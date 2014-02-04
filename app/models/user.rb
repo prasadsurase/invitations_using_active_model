4490b9e5bd5a18b08c8aee939914705483ee6e24
Prasad Surase <prasad@joshsoftware.com>
2014-02-04 17:42:42 +0530

    added settings for devise invitable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   ## Database authenticatable
